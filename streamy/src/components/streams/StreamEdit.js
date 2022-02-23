441964133dafee95b4add7dd6fb14ea57b03afac
gaka444 <vgala677@gmail.com>
2022-02-23 22:36:20 +0530

    router changes

--- streamy/src/components/streams/StreamEdit.js
+++ streamy/src/components/streams/StreamEdit.js
@@ -1,15 +1,23 @@
 import React from "react";
 import {connect} from 'react-redux';
 import {fetchStream} from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component  {
   componentDidMount(){
     this.props.fetchStream(this.props.match.params.id);
   }
+
+  onSubmit = (formValues)=>{
+    console.log(formValues)
+  }
   render(){
     if(!this.props.stream)
       return <div>Loading...</div>
-    return <div>{this.props.stream.title}</div>;
+    return (<div>
+      <h3>Edit a Stream</h3>
+      <StreamForm onSubmit={this.onSubmit} />
+      </div>);
   }
 };
 
